d5e0350ea78bfabd15e8fd9500abaaa4db40c5b4
Ashhar909 <ashhar374@gmail.com>
2022-09-14 09:16:45 +0530

    auth added

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const route = express.Router();
 const controller = require('../controller/controller');
-
+const authController = require('../controller/authController');
 
 route.post('/career',controller.DataCareer);
+route.post('/create-user',authController.create);
+route.get('/login', authController.login);
 
 module.exports = route;
